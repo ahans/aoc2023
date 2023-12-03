b10b83468772aa9354a196bda88b1c01ba1fd003
Alexander Hans <ahans@users.noreply.github.com>
2023-12-03 11:13:09 +0100

    Format files

--- python/day01.py
+++ python/day01.py
@@ -4,14 +4,23 @@ p1 = 0
 p2 = 0
 
 for line in open("../inputs/01.txt").readlines():
-    first = min((index, i + 1) for i in range(9) if (index := line. find(str(i + 1))) != -1)
-    last  = max((index, i + 1) for i in range(9) if (index := line.rfind(str(i + 1))) != -1)
+    first = min(
+        (index, i + 1) for i in range(9) if (index := line.find(str(i + 1))) != -1
+    )
+    last = max(
+        (index, i + 1) for i in range(9) if (index := line.rfind(str(i + 1))) != -1
+    )
     p1 += 10 * first[1] + last[1]
 
-    first_2 = min([(index, i + 1) for i, w in enumerate(words) if (index := line. find(w)) != -1] + [first])
-    last_2  = max([(index, i + 1) for i, w in enumerate(words) if (index := line.rfind(w)) != -1] + [last ])
+    first_2 = min(
+        [(index, i + 1) for i, w in enumerate(words) if (index := line.find(w)) != -1]
+        + [first]
+    )
+    last_2 = max(
+        [(index, i + 1) for i, w in enumerate(words) if (index := line.rfind(w)) != -1]
+        + [last]
+    )
     p2 += 10 * first_2[1] + last_2[1]
 
 print(p1)
 print(p2)
-
