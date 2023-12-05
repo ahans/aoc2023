af163c8eea7a836b8cd4a08306ea38058138dbb8
Alexander Hans <ahans@users.noreply.github.com>
2023-12-05 23:15:55 +0100

    Fix day 5 bug
    Non-matched parts of the range were not considered, leading to a wrong
    answer for the sample input.

--- python/day05.py
+++ python/day05.py
@@ -12,6 +12,7 @@ for m in lines[1:]:
     for mapping in m.split("\n")[1:]:
         dst_range_start, src_range_start, range_length = [int(x) for x in mapping.split()]
         stages[-1].append(Mapping(src=src_range_start, dst=dst_range_start, length=range_length))
+    stages[-1].sort()
 
 
 def overlap(b0, len0, b1, len1):
@@ -28,14 +29,22 @@ def expand_range(begin, length, stage_id):
     if stage_id == len(stages):
         return begin
     matches = []
+    match_ranges = []
     for mapping in stages[stage_id]:
         o = overlap(begin, length, mapping.src, mapping.length)
         if o:
             overlap_begin, overlap_length = o
+            match_ranges.append((overlap_begin, overlap_length))
             overlap_begin += mapping.dst - mapping.src
             matches.append(expand_range(overlap_begin, overlap_length, stage_id + 1))
     if not matches:
         return expand_range(begin, length, stage_id + 1)
+
+    for match_x, match_len in match_ranges:
+        if begin < match_x:
+            matches.append(expand_range(begin, match_x - begin, stage_id + 1))
+        begin = match_x + match_len
+
     return min(matches)
 
 
