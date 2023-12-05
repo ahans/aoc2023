4bfc88db8789377c3a96c302c3e9bc648b8438e2
Alexander Hans <ahans@users.noreply.github.com>
2023-12-05 13:56:57 +0100

    Add non-bruteforce day 05 solution

--- python/day05.py
+++ python/day05.py
@@ -1,37 +1,44 @@
-import sys
+from collections import namedtuple
 
 
-lines = sys.stdin.read().strip().split("\n\n")
+Mapping = namedtuple("Mapping", "src dst length")
+
+
+lines = open("../inputs/05.txt").read().strip().split("\n\n")
 seeds = [int(x) for x in lines[0].split()[1:]]
-maps = {}
-src_dst_map = {}
+stages = []
 for m in lines[1:]:
-    m = m.split("\n")
-    name = m[0].split()[0]
-    src, dst = name.split("-to-")
-    assert src not in src_dst_map
-    src_dst_map[src] = dst
-    maps[f"{src}-{dst}"] = []
-    for mapping in m[1:]:
-        dst_range_start, src_range_start, dst_length = [int(x) for x in mapping.split()]
-        maps[f"{src}-{dst}"].append((src_range_start, dst_range_start, dst_length))
+    stages.append([])
+    for mapping in m.split("\n")[1:]:
+        dst_range_start, src_range_start, range_length = [int(x) for x in mapping.split()]
+        stages[-1].append(Mapping(src=src_range_start, dst=dst_range_start, length=range_length))
 
-min_location = 999999999999
-for s, length in zip(seeds[0::2], seeds[1::2]):
-    for seed in range(s, s + length):
-        src = "seed"
-        n = seed
-        while src != "location":
-            dst = src_dst_map[src]
-            found = False
-            for src_range_start, dst_range_start, length in maps[f"{src}-{dst}"]:
-                if src_range_start <= n < src_range_start + length:
-                    found = True
-                    offset_in_src = n - src_range_start
-                    assert offset_in_src >= 0
-                    n = dst_range_start + offset_in_src
-                    break
-            src = dst
-        print(f"seed={s} {n}")
-        min_location = min(min_location, n)
-print(min_location)
+
+def overlap(b0, len0, b1, len1):
+    if b0 > b1:
+        b0, b1, len0, len1 = b1, b0, len1, len0
+    b, e = max(b0, b1), min(b0 + len0, b1 + len1)
+    if e > b:
+        return b, e - b
+    else:
+        return None
+
+
+def expand_range(begin, length, stage_id):
+    if stage_id == len(stages):
+        return begin
+    matches = []
+    for mapping in stages[stage_id]:
+        o = overlap(begin, length, mapping.src, mapping.length)
+        if o:
+            have_overlap = True
+            overlap_begin, overlap_length = o
+            overlap_begin += mapping.dst - mapping.src
+            matches.append(expand_range(overlap_begin, overlap_length, stage_id + 1))
+    if not matches:
+        return expand_range(begin, length, stage_id + 1)
+    return min(matches)
+
+
+print(min(expand_range(s, 1, 0) for s in seeds))
+print(min(expand_range(s, l, 0) for s, l in zip(seeds[0::2], seeds[1::2])))
