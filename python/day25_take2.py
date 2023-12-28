6159a82f4a9dfe982d31c1310b88b062c0bffb3c
Alexander Hans <ahans@users.noreply.github.com>
2023-12-28 21:45:50 +0100

    add random sampling

--- python/day25_take2.py
+++ python/day25_take2.py
@@ -17,30 +17,34 @@ def normalize(u, v):
 
 
 flow = defaultdict(int)
-for s in graph.keys():
+while True:
+    vertices = random.sample(list(graph.keys()), 20)
+    for s in vertices:
+        seen = set()
+        q = [(0,s)]
+        max_dist = defaultdict(lambda: sys.maxsize)
+        while q:
+            steps, u = q.pop(0)
+            if u in seen:
+                continue
+            seen.add(u)
+            for v in graph[u]:
+                if v not in seen and steps + 1 < max_dist[v]:
+                    flow[normalize(u,v)] += 1
+                    max_dist[v] = steps + 1
+                    q.append((steps + 1, v))
+    flow_pairs = [(v, k) for k, v in flow.items()]
+    flow_pairs.sort(reverse=True)
+    cut = set([pair for _, pair in flow_pairs[:3]])
+
+    q = [list(graph.keys())[0]]
     seen = set()
-    q = [(0,s)]
-    max_dist = defaultdict(lambda: sys.maxsize)
     while q:
-        steps, u = q.pop(0)
-        if u in seen:
-            continue
+        u = q.pop(0)
         seen.add(u)
         for v in graph[u]:
-            if v not in seen and steps + 1 < max_dist[v]:
-                flow[normalize(u,v)] += 1
-                max_dist[v] = steps + 1
-                q.append((steps + 1, v))
-flow = [(v, k) for k, v in flow.items()]
-flow.sort(reverse=True)
-cut = set([pair for _, pair in flow[:3]])
-
-q = [list(graph.keys())[0]]
-seen = set()
-while q:
-    u = q.pop(0)
-    seen.add(u)
-    for v in graph[u]:
-        if v not in seen and (u,v) not in cut and (v,u) not in cut:
-            q.append(v)
-print(len(seen) * (len(graph.keys()) - len(seen)))
+            if v not in seen and (u,v) not in cut and (v,u) not in cut:
+                q.append(v)
+    if len(seen) < len(graph.keys()):    
+        print(len(seen) * (len(graph.keys()) - len(seen)))
+        break
